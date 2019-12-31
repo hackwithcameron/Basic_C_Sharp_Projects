1a772889a8dd1da4877e139e8046dca64eb62d65
Cameron <hackwithcameron@gamil.com>
2019-12-31 14:39:45 -0800

    Boolean Practice
    Application Questionnaire practice using Boolean values.

---
+++ BooleanDrill/BooleanDrill/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BooleanDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            byte age;
+            bool dui;
+            byte tickets;
+            //bool qualified;
+
+
+
+            //***Get users age***
+
+            Console.WriteLine("What is your age?");
+            userInput = Console.ReadLine();
+            //Convert userInput to byte.
+            age = Convert.ToByte(userInput);
+
+
+
+            //***Ask if user has any DUIs***
+
+            Console.WriteLine("Have you ever had a DUI? Answer 'true' or 'false'");
+            userInput = Console.ReadLine();
+            //Convert userInput to boolean.
+            dui = Convert.ToBoolean(userInput);
+
+
+
+            //***Ask if user has any Speeding Tickets***
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            userInput = Console.ReadLine();
+            //Convert userInput to Byte.
+            tickets = Convert.ToByte(userInput);
+
+
+
+            //***Determine if Applicant is qualified***
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine((age > 15) && (dui != true) && (tickets <= 3));
+        }
+    }
+}
