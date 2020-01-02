f03d2522e2da14431282d006c47559c2722a3f73
Cameron <hackwithcameron@gamil.com>
2020-01-02 11:48:31 -0800

    Practice with StringBuilder
    Practice using stringbuilder.

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace String
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userInput;
+            StringBuilder answer = new StringBuilder();
+
+
+
+            //Favorite Color
+            Console.WriteLine("What is yuor favorite color?");
+            userInput = Console.ReadLine();
+            answer.Append("Your favorite color is " + userInput.ToUpper() + ". ");
+
+            //Favorite number
+            Console.WriteLine("What is yuor favorite number?");
+            userInput = Console.ReadLine();
+            answer.Append("Your favorite number is " + userInput + ". ");
+
+            //Favorite Food
+            Console.WriteLine("What is yuor favorite food?");
+            userInput = Console.ReadLine();
+            answer.Append("Your favorite food is " + userInput.ToUpper() + ". ");
+
+            //Print response
+            Console.WriteLine(answer);
+        }
+    }
+}
