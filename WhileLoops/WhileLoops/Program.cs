bb4ce38200c98dbddb4bccfb0054580e5ae93436
Cameron <hackwithcameron@gamil.com>
2020-01-02 10:32:29 -0800

    While and do while loops
    Practice using while loops.

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userInput;
+            string name;
+            bool stop;
+            byte num;
+            bool guess;
+
+
+            //Get users name and welcome user. If no input is given the question is looped.
+            stop = false;
+
+            while (!stop)
+            {
+                Console.WriteLine("What is your name?");
+                name = Console.ReadLine();
+
+                if (name != "")
+                {
+                    Console.WriteLine("Welcome, " + name.ToString());
+                    stop = true;
+                }
+            }
+
+            //User guesses a number between 1 and 10.
+            Console.WriteLine("Guess a number between 1 and 10");
+            userInput = Console.ReadLine();
+            num = Convert.ToByte(userInput);
+            guess = num == 7;
+
+
+            do
+            {
+                switch (num)
+                {
+                    case 7:
+                        Console.WriteLine("Congratulations!!! You guessed the number 7 correctly");
+                        guess = true;
+                        break;
+                    default:
+                        Console.WriteLine(num + " is not the number we are looking for.");
+                        Console.WriteLine("Guess a number between 1 and 10");
+                        userInput = Console.ReadLine();
+                        num = Convert.ToByte(userInput);
+                        break;
+                }
+            } while (!guess);
+        }
+    }
+}
