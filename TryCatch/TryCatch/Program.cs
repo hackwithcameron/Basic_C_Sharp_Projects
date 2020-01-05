e20e62580e1ca3fc50b426c68f993a2bfbff3d32
Cameron <hackwithcameron@gamil.com>
2020-01-04 16:22:28 -0800

    TryCatch
    Practice using try and catch for exception handling.

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum;
+            List<int> nums = new List<int> { 3, 6, 8, 14, 53, 61, 128 };
+            decimal answer;
+            bool stop;
+
+            stop = false;
+
+
+            while (!stop)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a number to be divided by ");
+                    foreach (int num in nums)
+                    {
+                        string comma = ", ";
+                        Console.Write(num + comma);
+                        if (nums.IndexOf(num) == nums.Count - 1)
+                        {
+                            comma.TrimEnd(',', ' ');
+                            Console.Write("\n>>>  ");
+                        }
+                    }
+                    userNum = Convert.ToInt32(Console.ReadLine());
+
+                    foreach (int num in nums)
+                    {
+                        //Converts numbers to decimal so returned answer will be decimal
+                        answer = Convert.ToDecimal(num) / userNum;
+                        Console.WriteLine(num + " Divided by " + userNum + " = " + answer);
+                        stop = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "\n");
+                }
+            }
+  
+        }
+    }
+}
