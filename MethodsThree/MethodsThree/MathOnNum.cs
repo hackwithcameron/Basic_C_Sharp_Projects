4e13544a30c9b6d646e3c5059884aa000769e6d5
Cameron <hackwithcameron@gamil.com>
2020-01-06 10:57:04 -0800

    Practice with methods
    Practice moving inputs in and out of methods

---
+++ MethodsThree/MethodsThree/MathOnNum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MethodsThree
+{
+    public class MathOnNum
+    {
+        int answer;
+        decimal answer2;
+        public MathOnNum()
+        {
+        }
+
+        public int Math(int userNum)
+        {
+            answer = userNum + 15;
+            return answer;
+        }
+
+        public decimal Math(decimal userNum)
+        {
+            answer2 = userNum / 25;
+            return answer2;
+        }
+
+        public int Math(string userNum)
+        {
+            answer = Convert.ToInt32(userNum);
+            return answer * 2;
+        }
+    }
+}
---
+++ MethodsThree/MethodsThree/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodsThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int userNum;
+            int answer;
+            decimal userNum2;
+            decimal answer2;
+            MathOnNum math = new MathOnNum();
+            
+
+            // First method adds 15 to int userInput
+            Console.WriteLine("Enter a whole number");
+            userNum = Convert.ToInt32(Console.ReadLine());
+            answer = math.Math(userNum);
+            Console.WriteLine(userNum + " Plus 15 = " + answer);
+
+            // Second method Divideds decimal userInput by 25
+            Console.WriteLine("Enter another number with decimal");
+            userNum2 = Convert.ToDecimal(Console.ReadLine());
+            answer2 = math.Math(userNum2);
+            Console.WriteLine(userNum2 + " Divided by 25 = " + answer2);
+
+            // Takes string userInput and returns int * 2
+            Console.WriteLine("Enter one last whole number");
+            userInput = Console.ReadLine();
+            try
+            {
+                answer = math.Math(userInput);
+                Console.WriteLine(userInput + " Times 2 = " + answer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
