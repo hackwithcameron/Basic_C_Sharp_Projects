3f6dae3ffe867db8e01e410a4d08f113b780df30
Cameron <hackwithcameron@gamil.com>
2020-01-05 19:34:32 -0800

    Tip Calculator
    Made a quick tip calculator for practice with methods.

---
+++ Methods/Methods/MathForNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Methods
+{
+    public class MathForNum
+    {
+        public MathForNum()
+        {
+        }
+
+        public decimal userNum { get; }
+        public decimal answer;
+        
+        
+        public decimal MathOne(decimal userNum)
+        {
+            answer = userNum * 10/100;
+            return answer;
+        }
+
+        public decimal MathTwo(decimal userNum)
+        {
+            answer = userNum * 15 / 100;
+            return answer;
+        }
+
+        public decimal MathThree(decimal userNum)
+        {
+            answer = userNum * 20 / 100;
+            return answer;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal userNum;
+            MathForNum math = new MathForNum();
+            decimal answer;
+            decimal answer2;
+            decimal answer3;
+            
+
+            Console.WriteLine("Enter bill total:");
+            userNum = Convert.ToDecimal(Console.ReadLine());
+            answer = math.MathOne(userNum);
+            answer2 = math.MathTwo(userNum);
+            answer3 = math.MathThree(userNum);
+            Console.WriteLine("10% of " + userNum.ToString("C") + " is " + answer.ToString("C"));
+            Console.WriteLine("15% of " + userNum.ToString("C") + " is " + answer2.ToString("C"));
+            Console.WriteLine("20% of " + userNum.ToString("C") + " is " + answer3.ToString("C"));
+        }
+    }
+}
