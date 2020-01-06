b94f8f93144caac58f002914c01c7c58f7733714
Cameron <hackwithcameron@gamil.com>
2020-01-06 15:30:43 -0800

    Method Output Parameters
    Practice with method output parameters

---
+++ MethodOutputParameter/MethodOutputParameter/DivideTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MethodOutputParameter
+{
+    public static class DivideTwo
+    {
+        public static void Divide(int userNum)
+        {
+            Console.WriteLine(userNum + " Divided by 2 = " + (userNum / 2));
+        }
+
+        public static void Divide(out decimal userNum, out decimal userNum2, out decimal answer)
+        {
+            Console.WriteLine("Now choose two numbers to be divided into eachother");
+
+            Console.WriteLine("Enter first number");
+            userNum = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            userNum2 = Convert.ToDecimal(Console.ReadLine());
+
+            answer = userNum / userNum2;
+        }
+    }
+}
---
+++ MethodOutputParameter/MethodOutputParameter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodOutputParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum;
+            decimal userNum1;
+            decimal userNum2;
+            decimal answer;
+
+
+
+            Console.WriteLine("Please enter a number to be divided by 2");
+            userNum = Convert.ToInt32(Console.ReadLine());
+            DivideTwo.Divide(userNum);
+
+            
+            DivideTwo.Divide(out userNum1, out userNum2, out answer);
+            Console.WriteLine(userNum + " Divided by " + userNum2 + " = " + answer);
+        }
+    }
+}
