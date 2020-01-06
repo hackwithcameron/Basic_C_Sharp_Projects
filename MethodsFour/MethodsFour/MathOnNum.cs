0bf2a825b33d1f640b18667cc1bf83f9f05969b6
Cameron <hackwithcameron@gamil.com>
2020-01-06 11:57:22 -0800

    Default Parameter
    Using  default Parameters in Methods

---
+++ MethodsFour/MethodsFour/MathOnNum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MethodsFour
+{
+    public class MathOnNum
+    {
+        decimal answer;
+
+        public MathOnNum()
+        {
+        }
+
+        public decimal Math(int userInput, int userInput2 = 1)
+        {
+            answer = userInput * userInput2;
+            return answer;
+        }
+    }
+}
---
+++ MethodsFour/MethodsFour/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodsFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum;
+            int userNum2;
+            decimal answer;
+            MathOnNum math = new MathOnNum();
+
+
+            Console.WriteLine("This is a Multiplication Calculator \n");
+            Console.WriteLine("You will be asked to enter two numbers \n***First number times the Second number***");
+
+            //First Number
+            Console.WriteLine("What is your first number?");
+            userNum = Convert.ToInt32(Console.ReadLine());
+
+
+            try
+            {
+                //Second Number
+                Console.WriteLine("What is your second number? Default is 1");
+                userNum2 = Convert.ToInt32(Console.ReadLine());
+
+                answer = math.Math(userNum, userNum2);
+                Console.WriteLine(userNum + " times " + userNum2 + " = " + answer);
+            }
+            catch (Exception)
+            {
+                answer = math.Math(userNum);
+                Console.WriteLine(userNum + " times 1 = " + answer);
+            }
+        }
+    }
+}
