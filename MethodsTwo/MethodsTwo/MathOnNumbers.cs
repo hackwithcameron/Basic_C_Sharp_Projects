63912f05982512a2700c5d46d4de77772d208c90
Cameron <hackwithcameron@gamil.com>
2020-01-05 20:35:55 -0800

    Void Method
    Practice with using void methods.

---
+++ MethodsTwo/MethodsTwo/MathOnNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MethodsTwo
+{
+    public class MathOnNumbers
+    {
+        public MathOnNumbers()
+        {
+        }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public static void MathOne(int A, int B)
+        {
+            int answer;
+            answer = A * 2;
+            Console.WriteLine("Second number is " + B);
+            Console.WriteLine("First number " + "\"" + A + "\"" + " x 2 = " + answer);
+        }
+    }
+}
---
+++ MethodsTwo/MethodsTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodsTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum1;
+            int userNum2;
+            MathOnNumbers math = new MathOnNumbers();
+            
+
+            Console.WriteLine("Please enter a number:");
+            userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number:");
+            userNum2 = Convert.ToInt32(Console.ReadLine());
+            math.A = userNum1;
+            math.B = userNum2;
+            MathOnNumbers.MathOne(math.A, math.B);
+
+        }
+    }
+}
