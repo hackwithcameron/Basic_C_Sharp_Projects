2aa8fc6a66313b627b0166d36ed934dabf898049
Cameron <hackwithcameron@gamil.com>
2020-01-09 15:56:50 -0800

    Struct
    Using Struct

---
+++ Struct/Struct/Number.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Struct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 3.14m };
+            
+
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
