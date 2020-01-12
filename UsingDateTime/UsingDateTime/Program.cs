bee8b2a135ca4f3b1e7ec2d45e514e41f3976ac1
Cameron <hackwithcameron@gamil.com>
2020-01-11 17:29:20 -0800

    DateTime
    Adding a user input of hour to current date

---
+++ UsingDateTime/UsingDateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UsingDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+            Console.WriteLine("Please enter a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            // Adds userNum to date
+            DateTime answer = date.AddHours(userNum);
+            Console.WriteLine("In {0} hours it will be {1}", userNum, answer);
+        }
+    }
+}
