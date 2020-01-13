10856a7f0c6dd43ef638830d75e8da97c4c95e41
Cameron <hackwithcameron@gamil.com>
2020-01-13 15:25:40 -0800

    Define Variables
    Use of different keywords to define different variables

---
+++ Keywords/Keywords/PhoneBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Keywords
+{
+    public class PhoneBook
+    {
+        public PhoneBook(string name) : this(name, "0000000000")
+        {
+
+        }
+        public PhoneBook(string name, string num)
+        {
+            Name = name;
+            Num = num;
+        }
+        
+        public string Name { get; set; }
+        public string Num { get; set; }
+        public Dictionary<string, string> DictionaryOfNums = new Dictionary<string, string>();
+
+        public void Confirm(string name, string num)
+        {
+            DictionaryOfNums.Add(name, num);
+            Console.WriteLine("The contact {0} with the phone number {1} has been saved to the contacts list.", name, num);
+
+            foreach (KeyValuePair<string, string> kvp in DictionaryOfNums)
+            {
+                Console.WriteLine("PhoneBook:\nName: {0}\nNumber: {1}", kvp.Key, kvp.Value);
+            }
+        }
+    }
+}
---
+++ Keywords/Keywords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Keywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string appName = "FoneBook";
+            string num;
+            string userInput;
+
+            Console.WriteLine("Welcome to {0}", appName);
+            Console.WriteLine("What is the name of the contact you would like to save?");
+            userInput = Console.ReadLine();
+            Console.WriteLine("What is the phone number of the contact you would like to save?");
+            num = Console.ReadLine();
+
+            var contact = new PhoneBook(userInput, num);
+            contact.Confirm(contact.Name, contact.Num);
+            
+        }
+    }
+}
